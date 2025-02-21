d7fdfca2606c51963296217131b58cf4bd45aef5
Afzaal Ahmad <92163453+AfzaalEFFI@users.noreply.github.com>
2025-02-21 18:44:39 +0500

    blog section about page

--- app/components/AboutComponents/GroupAbout.jsx
+++ app/components/AboutComponents/GroupAbout.jsx
@@ -5,12 +5,12 @@ import GigaComponent from "../GigaComponent";
 const GroupAbout = () => {
   return (
     <div className=" px-[75px] py-[120px] ">
-      <div className="bg-grpupCover bg-cover h-[500px] w-full flex items-end">
-        <div className="bg-[rgb(237,237,237,0.9)] backdrop-blur-md space-y-4 w-[490px] px-9 py-5 rounded-tr-xl">
+      <div className="bg-grpupCover bg-cover h-[550px] w-full flex items-end rounded-[10px]">
+        <div className="bg-[rgb(237,237,237,0.9)] backdrop-blur-md space-y-4 w-[490px] px-9 py-5 rounded-tr-xl rounded-bl-md">
           <p>
             Great institutions are built over time, nurturing dreams and
             fostering visions that promote peaceful and inclusive societies for
-            sustainable development. Giga Group is one such institution.{" "}
+            sustainable development. Giga Group is one such institution.
           </p>
 
           <GigaComponent title="Najeeb Amin Pardesi" />
