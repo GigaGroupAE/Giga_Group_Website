e3c69516ef091ae80c65bc7a62e392800a38b172
Afzaal Ahmad <92163453+AfzaalEFFI@users.noreply.github.com>
2025-03-12 16:13:56 +0500

    Complete ProjectPaegui

---
+++ src/Data/DeliveredProjectsData.js
@@ -0,0 +1,59 @@
+export const deliveredData = [
+  {
+    title: "Project Name",
+    status: "Delivered",
+    description:
+      "An ultra-modern 40-story residential tower located in the Jumeirah Lake Towers (JLT) community.",
+    image: "gcv1.webp",
+  },
+  {
+    title: "Goldcrest Views 1",
+    status: "Delivered",
+    description:
+      "An ultra-modern 40-story residential tower located in the Jumeirah Lake Towers (JLT) community.",
+    image: "gcv1.webp",
+  },
+  {
+    title: "Goldcrest Views 2",
+    status: "Delivered",
+    description:
+      "An ultra-modern 40-story residential tower located in the Jumeirah Lake Towers (JLT) community.",
+    image: "gcv1.webp",
+  },
+  {
+    title: "Goldcrest Executive",
+    status: "Delivered",
+    description:
+      "An ultra-modern 40-story residential tower located in the Jumeirah Lake Towers (JLT) community.",
+    image: "gcv1.webp",
+  },
+  {
+    title: "Pearl Bliss Residence",
+    status: "Delivered",
+    description:
+      "An ultra-modern 40-story residential tower located in the Jumeirah Lake Towers (JLT) community.",
+    button: "Learn More",
+    image: "gcv1.webp",
+  },
+  {
+    title: "Goldcrest Views 1",
+    status: "Delivered",
+    description:
+      "An ultra-modern 40-story residential tower located in the Jumeirah Lake Towers (JLT) community.",
+    image: "gcv1.webp",
+  },
+  {
+    title: "Goldcrest Views 2",
+    status: "Delivered",
+    description:
+      "An ultra-modern 40-story residential tower located in the Jumeirah Lake Towers (JLT) community.",
+    image: "goldcrest-views-2-b.jpg",
+  },
+  {
+    title: "Goldcrest Executive",
+    status: "Delivered",
+    description:
+      "An ultra-modern 40-story residential tower located in the Jumeirah Lake Towers (JLT) community.",
+    image: "goldcrest-executive-b.jpg",
+  },
+];
