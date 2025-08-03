d2b3e5575a184cbd240755a194918295573fb6d4
Afzaal Ahmad <92163453+AfzaalEFFI@users.noreply.github.com>
2025-08-03 18:30:51 +0500

    build fix

--- app/ocean-crest/page.jsx
+++ app/ocean-crest/page.jsx
@@ -1,85 +1,92 @@
-import React from "react";
-import OceanCrestHero from "../components/ProjectsPages/OceanCrestHero";
-import Image from "next/image";
-import bed from "../../public/bed.svg";
-import { projectData } from "@/src/Data/ProjectData";
-import { projectImages } from "@/src/Data/ProjectImages";
-import map from "../../public/projectMap.svg";
-import GigaComponent from "../components/GigaComponent";
-import ButtonPrimary from "../components/ButtonPrimary";
-import ButtonSecandary from "../components/ButtonSecandary";
+// import React from "react";
+// import OceanCrestHero from "../components/ProjectsPages/OceanCrestHero";
+// import Image from "next/image";
+// import bed from "../../public/bed.svg";
+// import { projectData } from "@/src/Data/ProjectData";
+// import { projectImages } from "@/src/Data/ProjectImages";
+// import map from "../../public/projectMap.svg";
+// import GigaComponent from "../components/GigaComponent";
+// import ButtonPrimary from "../components/ButtonPrimary";
+// import ButtonSecandary from "../components/ButtonSecandary";
 
-import projectImage from "../../public/aboutGiga1.webp";
-import { amenites } from "@/src/Data/Amenities";
-import ContactUsForm from "../components/ContactUsForm";
-import AboutProject from "../components/ProjectsPages/AboutProject";
-import ProjectGallery from "../components/ProjectsPages/ProjectGallery";
-import ProjectLocation from "../components/ProjectsPages/ProjectLocation";
+// import projectImage from "../../public/aboutGiga1.webp";
+// import { amenites } from "@/src/Data/Amenities";
+// import ContactUsForm from "../components/ContactUsForm";
+// import AboutProject from "../components/ProjectsPages/AboutProject";
+// import ProjectGallery from "../components/ProjectsPages/ProjectGallery";
+// import ProjectLocation from "../components/ProjectsPages/ProjectLocation";
+
+// const page = () => {
+//   return (
+//     <section>
+//       <div>
+//         <OceanCrestHero />
+//       </div>
+//       <AboutProject />
+//       <ProjectGallery />
+
+//       <ProjectLocation />
+
+//       <div className="bg-gradient-to-tr py-16 ">
+//         <div className="mx-auto w-2/4 items-center">
+//           <h2 className="text-center subHeading">
+//             Investing in <b>Everything you Need</b>
+//           </h2>
+//           <p className="text-center descriptionText">
+//             Great institutions are built over time, nurturing dreams and
+//             fostering visions that promote peaceful and inclusive societies for
+//             sustainable development. Giga Group is one such institution.
+//           </p>
+//         </div>
+
+//         <div className=" gap-2 container mx-auto px-32 mt-16 grid md:grid-cols-4">
+//           {amenites?.map((item) => {
+//             return (
+//               <div className="bg-indigo-100 space-y-4 rounded-md  py-10 items-center flex-col  justify-center flex">
+//                 <Image
+//                   src={item.image}
+//                   width={25}
+//                   height={23}
+//                   alt={item.title}
+//                   className="  rounded-md   object-cover"
+//                 />
+//                 <h2 className="descriptionText ">Hello</h2>
+//               </div>
+//             );
+//           })}
+//         </div>
+//       </div>
+
+//       <div className="bg-secondary">
+//         <div
+//           className="w-4/6 mx-auto py-16  space-y-2
+//         "
+//         >
+//           <h1 className="text-center text-white font-poppins text-4xl font-semibold ">
+//             Why Choose <b>Pearl Bliss Residence?</b>{" "}
+//           </h1>
+//           <p className="text-center text-white font-poppins text-sm ">
+//             Pearl Bliss Residence by Giga Group offers a lifestyle like no
+//             other, set in a prime location that caters to your every need.
+//             Surrounded by lush parks, top-rated schools, nurseries, and a
+//             diverse range of restaurants and retail outlets, this community is
+//             thoughtfully designed for families seeking comfort and convenience.
+//             With advanced security and daycare services, Pearl Bliss Residence
+//             ensures peace of mind for every resident.
+//           </p>
+//         </div>
+//       </div>
+
+//       <ContactUsForm />
+//     </section>
+//   );
+// };
+
+// export default page;
+import React from 'react';
 
 const page = () => {
-  return (
-    <section>
-      <div>
-        <OceanCrestHero />
-      </div>
-      <AboutProject />
-      <ProjectGallery />
-
-      <ProjectLocation />
-
-      <div className="bg-gradient-to-tr py-16 ">
-        <div className="mx-auto w-2/4 items-center">
-          <h2 className="text-center subHeading">
-            Investing in <b>Everything you Need</b>
-          </h2>
-          <p className="text-center descriptionText">
-            Great institutions are built over time, nurturing dreams and
-            fostering visions that promote peaceful and inclusive societies for
-            sustainable development. Giga Group is one such institution.
-          </p>
-        </div>
-
-        <div className=" gap-2 container mx-auto px-32 mt-16 grid md:grid-cols-4">
-          {amenites?.map((item) => {
-            return (
-              <div className="bg-indigo-100 space-y-4 rounded-md  py-10 items-center flex-col  justify-center flex">
-                <Image
-                  src={item.image}
-                  width={25}
-                  height={23}
-                  alt={item.title}
-                  className="  rounded-md   object-cover"
-                />
-                <h2 className="descriptionText ">Hello</h2>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-
-      <div className="bg-secondary">
-        <div
-          className="w-4/6 mx-auto py-16  space-y-2
-        "
-        >
-          <h1 className="text-center text-white font-poppins text-4xl font-semibold ">
-            Why Choose <b>Pearl Bliss Residence?</b>{" "}
-          </h1>
-          <p className="text-center text-white font-poppins text-sm ">
-            Pearl Bliss Residence by Giga Group offers a lifestyle like no
-            other, set in a prime location that caters to your every need.
-            Surrounded by lush parks, top-rated schools, nurseries, and a
-            diverse range of restaurants and retail outlets, this community is
-            thoughtfully designed for families seeking comfort and convenience.
-            With advanced security and daycare services, Pearl Bliss Residence
-            ensures peace of mind for every resident.
-          </p>
-        </div>
-      </div>
-
-      <ContactUsForm />
-    </section>
-  );
+  return <div>page</div>;
 };
 
 export default page;
