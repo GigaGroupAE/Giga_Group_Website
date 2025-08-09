f8db3bb385929a20495e7eede39afc703c6aebde
GigaGroupAE <gigaearth.dev@gmail.com>
2025-08-09 19:28:42 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar';
 import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import ChatwootWidget from './components/ChatwootWidget';
-import Head from 'next/head';
+
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,13 +27,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Head>
+        <head>
           <script
             src="https://analytics.ahrefs.com/analytics.js"
             data-key="4x8HziLCIVvUw2fBAoX4DA"
             async
           ></script>
-        </Head>
+        </head>
         <Navbar />
         <ChatwootWidget />
         <Analytics />
