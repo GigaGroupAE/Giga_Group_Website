35562ce2904134645f21329d7ab0b06414cde685
Afzaal Ahmad <92163453+AfzaalEFFI@users.noreply.github.com>
2025-08-09 19:14:18 +0500

    add ahref

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import ChatwootWidget from './components/ChatwootWidget';
+import Head from 'next/head';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -26,6 +27,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Head>
+          <script
+            src="https://analytics.ahrefs.com/analytics.js"
+            data-key="4x8HziLCIVvUw2fBAoX4DA"
+            async
+          ></script>
+        </Head>
         <Navbar />
         <ChatwootWidget />
         <Analytics />
