d54d242e1663b90fd0f1bbdd73bf2c1f4b9d3ac5
Afzaal Ahmad <92163453+AfzaalEFFI@users.noreply.github.com>
2025-08-11 19:04:34 +0500

    update script

--- app/components/Hero/HeroInputForm.jsx
+++ app/components/Hero/HeroInputForm.jsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import emailjs from '@emailjs/browser';
 import ButtonPrimary from '../ButtonPrimary';
 import { toast, ToastContainer } from 'react-toastify';
